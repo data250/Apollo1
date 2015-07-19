ef51c14ece48ec79dd12195a39968acd4a49b55c
Jacobs123 <jakubk92@gmail.com>
2015-07-19 13:03:10 +0200

    Kontrahenci

---
+++ src/main/Kontrahenci.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Kontrahenci{
+	
+	private static int id;
+	private String nazwa_firmy;
+	private String NIP;
+	private String REGON;
+	private String imie;
+	private String nazwisko;
+	private String  kod_pocztowy;
+	private String miasto;
+	private String ulica;
+	private String opis;
+	
+	
+	
+	
+	
+}
+
+	
+	
+	
+
+
